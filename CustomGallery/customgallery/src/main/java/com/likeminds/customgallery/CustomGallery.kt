9036196a16dc891afb6b3cb555422e168c617a26
Siddharth Dubey <siddharth.dubey@likeminds.community>
2023-05-16 16:14:32 +0530

    basic setup and dependencies

---
+++ CustomGallery/customgallery/src/main/java/com/likeminds/customgallery/CustomGallery.kt
@@ -0,0 +1,11 @@
+package com.likeminds.customgallery
+
+object CustomGallery {
+    fun initiateCustomGallery(
+        mediaTypes: List<String>,
+        isEditingEnabled: Boolean,
+        inputText: String
+    ) {
+
+    }
+}
