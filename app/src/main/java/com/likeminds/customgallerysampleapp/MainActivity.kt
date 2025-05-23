52a5fc2ad4fe656afcaa95290e6271f950409805
Siddharth Dubey <122088830+sidcr7-likeminds@users.noreply.github.com>
2025-05-23 13:02:44 +0530

    upgraded target sdk 35

--- app/src/main/java/com/likeminds/customgallerysampleapp/MainActivity.kt
+++ app/src/main/java/com/likeminds/customgallerysampleapp/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import com.likeminds.customgallery.CustomGallery
 import com.likeminds.customgallery.media.model.*
 import com.likeminds.customgallerysampleapp.databinding.ActivityMainBinding
@@ -29,6 +31,26 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { view, windowInsets ->
+            val innerPadding = windowInsets.getInsets(
+                // Notice we're using systemBars, not statusBar
+                WindowInsetsCompat.Type.systemBars()
+                        // Notice we're also accounting for the display cutouts
+                        or WindowInsetsCompat.Type.displayCutout()
+                        // If using EditText, also add
+                        or WindowInsetsCompat.Type.ime()
+            )
+            // Apply the insets as padding to the view. Here, set all the dimensions
+            // as appropriate to your layout. You can also update the view's margin if
+            // more appropriate.
+            view.setPadding(0, innerPadding.top, 0, innerPadding.bottom)
+
+            // Return CONSUMED if you don't want the window insets to keep passing down
+            // to descendant views.
+            WindowInsetsCompat.CONSUMED
+        }
+
         initClickListeners()
     }
 
